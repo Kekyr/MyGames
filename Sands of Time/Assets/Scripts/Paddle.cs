236145f1294d5301166dcf69642c30f3a2c66673
Mikhail <supermanmgdn@gmail.com>
2020-04-07 00:14:26 +0300

    Added game "Sands Of Time"
    Arkanoid

---
+++ Sands of Time/Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // configuration parameters
+    [SerializeField] float minX = 0.49f;
+    [SerializeField] float maxX = 15.4710f;
+    [SerializeField] float screenWidthInUnits = 16f;
+    
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float mousePosInUnits=Input.mousePosition.x/Screen.width*16;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
+    }
+}
---
+++ Sands of Time/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   public void LoadNextScene()
+   {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;//получаем индекс текущей сцены из build settings
+        SceneManager.LoadScene(currentSceneIndex + 1);
+   }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
+
